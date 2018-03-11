3ea7b15464f58371f0139aed8ed66b2aea571cc6
jkavdev <jhonatankolen@gmail.com>
2018-03-11 15:34:54 -0300

    Spring - 001 - adicionando projeto com spring e thymeleaf
    - template do materialize
    - criado apenas um index

---
+++ ed-groups-manager/src/main/java/br/com/jkavdev/edgroups/GroupsManagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.jkavdev.edgroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupsManagerApplication.class, args);
+	}
+}
